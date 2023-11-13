435d9a21d96e91f4db338bf5de8aebe4e1ea8f13
NRowley <nicholas.ryan.rowley@gmail.com>
2023-11-13 15:57:35 -0600

    Switch statements

--- Tutorial6/Source.cpp
+++ Tutorial6/Source.cpp
@@ -13,18 +13,25 @@ enum PlayerStatus {
 	PS_climbing
 };
 
+const float RUN_SPEED = 800.f;
+const float WALK_SPEED = 400.f;
+const float CROUCH_SPEED = 250.f;
+
 enum MovementStatus {
 	MS_crouched
 };
+
+void updateMovementSpeed(PlayerStatus PS, float& moveSpeed);
+void switchOnInt(int i);
 
 int main() {
 
 	//ENUMS///////////////////////////
 	PlayerStatus status;
 
-	status = PS_crouched;
+	//status = PS_crouched;
 
-	if (status == PS_crouched) {
+	/*if (status == PS_crouched) {
 		cout << "crouching \n";
 	}
 
@@ -32,7 +39,51 @@ int main() {
 
 	if (status == PS_crouched) {
 		cout << "crouching \n";
-	}
+	}*/
+
+	//Switch Statements///////////////
+	float moveSpeed;
+	int int1;
+
+	status = PS_walking;
+	status = PS_running;
+	
+	updateMovementSpeed(status, moveSpeed);
+	cout << "Move speed: " << moveSpeed << endl;
+	int1 = 10;
+	switchOnInt(int1);
 
 	system("pause");
+}
+
+void updateMovementSpeed(PlayerStatus PS, float& moveSpeed) {
+
+	switch (PS) {
+	case PS_walking:
+		moveSpeed = WALK_SPEED;
+		break;
+
+	case PS_running:
+		moveSpeed = RUN_SPEED;
+		break;
+
+	case PS_crouched:
+		moveSpeed = CROUCH_SPEED;
+	}
+
+}
+
+void switchOnInt(int i) {
+	switch (i) {
+	case 0:
+		cout << "Your number was zero. \n";
+		break;
+
+	case 1:
+		cout << "Your number was one. \n";
+		break;
+
+	default:
+		cout << "Your number was " << i << endl;
+	}
 }
