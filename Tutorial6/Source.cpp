01e0d7c79f073c84c287b245f00fa9705fb65089
NRowley <nicholas.ryan.rowley@gmail.com>
2023-11-13 15:27:09 -0600

    Enums

--- Tutorial6/Source.cpp
+++ Tutorial6/Source.cpp
@@ -2,11 +2,37 @@
 #include <string>
 using namespace std;
 
+enum PlayerStatus {
+	PS_crouched,
+	PS_standing,
+	PS_walking,
+	PS_running,
+	PS_sprinting,
+	PS_hiding,
+	PS_airborne,
+	PS_climbing
+};
 
+enum MovementStatus {
+	MS_crouched
+};
 
 int main() {
 
+	//ENUMS///////////////////////////
+	PlayerStatus status;
 
+	status = PS_crouched;
+
+	if (status == PS_crouched) {
+		cout << "crouching \n";
+	}
+
+	status = PlayerStatus::PS_airborne;
+
+	if (status == PS_crouched) {
+		cout << "crouching \n";
+	}
 
 	system("pause");
 }
